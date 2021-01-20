aca2ef55713305ba1ccb8000748b9d1a5c67195e
Iskander <g1683004@gmail.com>
2021-01-20 18:34:10 +0500

    Add the tables #3

--- php/php/queries/Most_popular_tours.php
+++ php/php/queries/Most_popular_tours.php
@@ -2,7 +2,7 @@
 include "../Utils.php";
 
 
-$query = "select type_tour, hotel, begin_date, end_date, count(*) as amount from tours join sales s on tours.id = s.tour_id group by tours.id order by amount desc ";
+$query = "select type_tour, hotel, begin_date, end_date, count(*) as amount from tours join sales s on tours.id = s.tour_id group by tours.id order by amount desc limit 1 ";
 echo "<p>Запрос: $query</p>";
 echo Utils::renderSelectQueryToTable($query, []);
 ?>
